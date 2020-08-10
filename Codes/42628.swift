604b68f4c65c240e0ac05a3e2f8d6f68a08cfc70
lidium <elesahich@gmail.com>
2020-08-11 00:42:05 +0900

    [Docs] 코드 업로드

---
+++ Codes/42628.swift
@@ -0,0 +1,42 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by elesahich on 2020/08/10.
+//  Copyright © 2020 elesahich. All rights reserved.
+//  42628
+
+import Foundation
+
+func solution(_ operations: [String]) -> [Int] {
+    var tempArray: [Int] = []
+    operations.map {
+        let firstValue = $0.first ?? nil
+        let lastValue = $0.split(separator: " ").last
+        
+        if firstValue == "I" {
+            let element = lastValue.map { last -> Int in return Int(last)! }!
+            tempArray.append(element)
+            tempArray.sort()
+        } else {
+            if lastValue == "1" && !tempArray.isEmpty {
+                tempArray.removeLast()
+            } else if lastValue == "-1" && !tempArray.isEmpty {
+                tempArray.removeFirst()
+            }
+        }
+    }
+    var resultArray: [Int] = []
+    resultArray.append(tempArray.max() ?? 0)
+    resultArray.append(tempArray.min() ?? 0)
+    
+    resultArray.sort(by: >)
+    if resultArray.isEmpty {
+        resultArray = [0,0]
+    }
+    return resultArray
+}
+
+print(solution(["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]))
+
+//print(solution(["I 16","D 1"]))
